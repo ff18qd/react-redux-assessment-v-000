4d11043ab233d40d221a65603c0c1e686b90bbec
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-05-17 02:51:13 +0000

    add NextstopCard class and css file

--- nextstop-client/src/containers/App.js
+++ nextstop-client/src/containers/App.js
@@ -6,13 +6,14 @@ const nextstops = [
     {
         name: "Sichuan",
         budget: 4500,
-        img_url: "test.com",
-        like: 0
+        img_url: "http://www.pctours.com/wp-content/uploads/2014/10/Sichuan4.jpg",
+        like: 0,
+        dislike: 0
     },
     {   
-        name: "Germany",
+        name: "Berlin",
         budget: 5500,
-        img_url: "test.com",
+        img_url: "https://lonelyplanetwp.imgix.net/2015/12/brandenburg-gate-berlin-GettyRF-1500-cs.jpg?crop=entropy&fit=crop&h=421&sharp=10&vib=20&w=748",
         like: 0,
         dislike: 0
     }
--- nextstop-client/src/containers/Nextstops.js
+++ nextstop-client/src/containers/Nextstops.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Nextstops.css';
 
 const Nextstops = (props) => (
     <div>
         <h1>Nextstops</h1>
-        { props.nextstops.map(n => <h3>{n.name}</h3>)}
+        { props.nextstops.map(n => 
+            <div className="NextstopCard">
+                <h3>{n.name}</h3>
+                <p>Budget: ${n.budget}</p>
+                <img src={n.img_url} alt={n.name} />
+                <p>Like: {n.like}</p>
+                <p>Dislike: {n.dislike}</p>
+            </div>
+            )}
     </div>
 )
     

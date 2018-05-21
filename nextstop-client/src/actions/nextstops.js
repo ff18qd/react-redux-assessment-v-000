4b89a15a9f116d95d934c73250cabb827c0a248a
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-05-21 22:23:41 +0000

    Fix nextstop-api cache repo

--- nextstop-client/src/actions/nextstops.js
+++ nextstop-client/src/actions/nextstops.js
@@ -10,7 +10,32 @@ const setNextstops = nextstops => {
 //use fetch data instead after fix the fetch api issue
 export const getNextstops = () => {
    return dispatch => {
-        let nextstops = [{name:"Yosamite National Park", budget: 800, img_url: "https://cdn.unifiedcommerce.com/content/product/large/628136609470.jpg", like: 0, dislike: 0}]
-        return dispatch(setNextstops(nextstops))
-    }
-}
+        // let nextstops = [{name:"Yosamite National Park", budget: 800, img_url: "https://cdn.unifiedcommerce.com/content/product/large/628136609470.jpg", like: 0, dislike: 0}]
+        
+        fetch("http://ec2-18-222-85-245.us-east-2.compute.amazonaws.com:8081/api/nextstops")
+        .then(response => response.json())
+        .then(nextstops => {
+            // console.log(nextstops);
+            return dispatch(setNextstops(nextstops)
+        )}
+            
+            // this.setState({nextstops})})
+        
+        // return dispatch(setNextstops(nextstops))
+    
+
+ // fetch("https://ec2-52-15-78-33.us-east-2.compute.amazonaws.com:8080/api/nextstops")
+    //     axios.get("https://80887eaf7a0b474e9fa06d513302b5b6.vfs.cloud9.us-east-2.amazonaws.com:8080/api/nextstops"
+    //         //, {
+    //         //   headers : { 
+    //         //     'Content-Type': 'application/json',
+    //         //     'Accept': 'application/json'
+    //         //   },
+    //         //   mode: 'no-cors',
+    //          // credentials: "include"
+    //         //}
+    //     )
+    //         .then(response => response.json())
+    //         .then(nextstops => {
+    //             console.log(nextstops);
+    //             this.setState({nextstops})})

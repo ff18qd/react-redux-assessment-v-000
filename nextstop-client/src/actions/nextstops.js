56a9df405cea239d3181aa6f387644c153c425f2
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-05-21 22:27:49 +0000

    client add fetch data in nextstops.js

--- nextstop-client/src/actions/nextstops.js
+++ nextstop-client/src/actions/nextstops.js
@@ -16,26 +16,9 @@ export const getNextstops = () => {
         .then(response => response.json())
         .then(nextstops => {
             // console.log(nextstops);
-            return dispatch(setNextstops(nextstops)
-        )}
-            
+        return dispatch(setNextstops(nextstops))})
+   }
+}
             // this.setState({nextstops})})
         
         // return dispatch(setNextstops(nextstops))
-    
-
- // fetch("https://ec2-52-15-78-33.us-east-2.compute.amazonaws.com:8080/api/nextstops")
-    //     axios.get("https://80887eaf7a0b474e9fa06d513302b5b6.vfs.cloud9.us-east-2.amazonaws.com:8080/api/nextstops"
-    //         //, {
-    //         //   headers : { 
-    //         //     'Content-Type': 'application/json',
-    //         //     'Accept': 'application/json'
-    //         //   },
-    //         //   mode: 'no-cors',
-    //          // credentials: "include"
-    //         //}
-    //     )
-    //         .then(response => response.json())
-    //         .then(nextstops => {
-    //             console.log(nextstops);
-    //             this.setState({nextstops})})

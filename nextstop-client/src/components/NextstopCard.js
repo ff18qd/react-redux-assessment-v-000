943d3948bef9a51f48e15fc30eaa899edbb76055
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-05-23 20:16:14 +0000

    add style to like dislike button having space left

--- nextstop-client/src/components/NextstopCard.js
+++ nextstop-client/src/components/NextstopCard.js
@@ -7,10 +7,14 @@ const NextstopCard = ({index, nextstop, handleOnDelete, handleOnLike, handleOnDi
         <h3>{nextstop.name}</h3>
         <img className="NextstopImage" src={nextstop.img_url} alt={nextstop.name} />
         <p>Budget: ${nextstop.budget}</p>
-        <button value={nextstop.id} onClick={(e) => handleOnLike(e)}>Like</button>
-        <p>Likes: {nextstop.like}</p>
-        <button value={nextstop.id} onClick={(e) => handleOnDislike(e)}>Disike</button>
-        <p>DisLikes: {nextstop.dislike}</p>
+        <div className="LikeDislike">
+            <button value={nextstop.id} onClick={(e) => handleOnLike(e)} style={{float: "left"}}>Like</button>
+            <p>{nextstop.like}</p>
+        </div>
+        <div className="LikeDislike">
+            <button value={nextstop.id} onClick={(e) => handleOnDislike(e)} style={{float: "left"}}>Disike</button>
+            <p>{nextstop.dislike}</p>
+        </div>
         <div>
             <button type="button" value={nextstop.id} onClick={(e) => handleOnDelete(e)}>Delete</button>
         </div>

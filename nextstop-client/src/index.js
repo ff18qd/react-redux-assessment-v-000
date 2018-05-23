a4859fe5988418be3f609020314f02770a6d3b28
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-05-23 20:43:52 +0000

    add two routes for home and contact us

--- nextstop-client/src/index.js
+++ nextstop-client/src/index.js
@@ -2,15 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-// import App from './containers/App';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import Home from './components/Home';
+import ContactUs from './components/ContactUs';
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/contactus"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >ContactUs</NavLink>
+  </div>;
+
+
 ReactDOM.render(
-    <Provider store={store}>
-        <App store={store}/>
-    </Provider>, 
+    <Router>
+    <div>
+        <Navbar />
+        <Route path="/" component={Home} />
+        <Route exact path="/contactus" component={ContactUs} />
+        <Provider store={store}>
+            <App store={store}/>
+        </Provider>
+     </div>
+     </Router>, 
     document.getElementById('root')
 );
 

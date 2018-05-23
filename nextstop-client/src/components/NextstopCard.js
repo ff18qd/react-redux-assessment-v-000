41c1ec823f8b431f6af3bee67dd1db3c0a17db72
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-05-23 00:54:58 +0000

    move NextstopCard to components folder

---
+++ nextstop-client/src/components/NextstopCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../containers/Nextstops.css';
+
+const NextstopCard = ({nextstop})=> (
+    <div key={nextstop.id} className="NextstopCard">
+        <h3>{nextstop.name}</h3>
+        <img className="NextstopImage" src={nextstop.img_url} alt={nextstop.name} />
+        <p>Budget: ${nextstop.budget}</p>
+        <button>Like</button>
+        <p>Likes: {nextstop.like}</p>
+        <button>Disike</button>
+        <p>DisLikes: {nextstop.dislike}</p>
+    </div>
+    );
+
+export default NextstopCard;

d23baaf0d0c80b0be7224d98fc886bbc73a3b9dc
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-05-23 01:39:44 +0000

    change Card to component and add a delete button

--- nextstop-client/src/containers/NextstopCard.js
+++ nextstop-client/src/containers/NextstopCard.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Nextstops.css';
 
 
-const NextstopCard = ({nextstop})=> (
-    <div key={nextstop.id} className="NextstopCard">
-        <h3>{nextstop.name}</h3>
-        <img className="NextstopImage" src={nextstop.img_url} alt={nextstop.name} />
-        <p>Budget: ${nextstop.budget}</p>
-        <button>Like</button>
-        <p>Likes: {nextstop.like}</p>
-        <button>Disike</button>
-        <p>DisLikes: {nextstop.dislike}</p>
-    </div>
-    );
+// const NextstopCard = ({nextstop})=> (
+//     <div key={nextstop.id} className="NextstopCard">
+//         <h3>{nextstop.name}</h3>
+//         <img className="NextstopImage" src={nextstop.img_url} alt={nextstop.name} />
+//         <p>Budget: ${nextstop.budget}</p>
+//         <button>Like</button>
+//         <p>Likes: {nextstop.like}</p>
+//         <button>Disike</button>
+//         <p>DisLikes: {nextstop.dislike}</p>
+//         <div>
+//             <button type="button" value="delete" onClick={this.deleteCard}>Delete</button>
+//         </div>
+//     </div>
+//     );
+
+
+
+class NextstopCard extends Component {
+    
+    
+    render() {
+        return (
+            <div key={this.props.nextstop.id} className="NextstopCard">
+                <h3>{this.props.nextstop.name}</h3>
+                <img className="NextstopImage" src={this.props.nextstop.img_url} alt={this.props.nextstop.name} />
+                <p>Budget: ${this.props.nextstop.budget}</p>
+                <button>Like</button>
+                <p>Likes: {this.props.nextstop.like}</p>
+                <button>Disike</button>
+                <p>DisLikes: {this.props.nextstop.dislike}</p>
+                <div>
+                    <button type="button" value="delete" onClick={this.deleteCard}>Delete</button>
+                </div>
+            </div>
+        )}
+}
+
+
 
 export default NextstopCard;

44ba41fb125fce05bcab28ca4b454744ee4e3e92
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-05-25 20:50:55 +0000

    add delete icon and center the icons in Card

--- nextstop-client/src/components/NextstopCard.js
+++ nextstop-client/src/components/NextstopCard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../containers/Nextstops.css';
 
 
 const NextstopCard = ({index, nextstop, handleOnDelete, handleOnLike, handleOnDislike})=> (
@@ -7,14 +6,14 @@ const NextstopCard = ({index, nextstop, handleOnDelete, handleOnLike, handleOnDi
         <h3>{nextstop.name}</h3>
         <img className="NextstopImage" src={nextstop.img_url} alt={nextstop.name} />
         <p>Budget: ${nextstop.budget}</p>
-        <div className="LikeDislike">
-            <button value={nextstop.id} onClick={(e) => handleOnLike(e)} ><i class="fa fa-thumbs-o-up"></i>: {nextstop.like}</button>
-        </div>
-        <div className="LikeDislike">
-            <button value={nextstop.id} onClick={(e) => handleOnDislike(e)} ><i class="fa fa-thumbs-o-down"></i>: {nextstop.dislike}</button>
+        <div>
+            <button value={nextstop.id} onClick={(e) => handleOnLike(e)} style={{fontSize:'20px'}}><i class="fa fa-thumbs-o-up"></i>: {nextstop.like}</button>
         </div>
         <div>
-            <button type="button" value={nextstop.id} onClick={(e) => handleOnDelete(e)}><i class="fa fa-close"></i> Delete</button>
+            <button value={nextstop.id} onClick={(e) => handleOnDislike(e)} style={{fontSize:'20px'}}><i class="fa fa-thumbs-o-down"></i>: {nextstop.dislike}</button>
+        </div>
+        <div>
+            <button type="button" value={nextstop.id} onClick={(e) => handleOnDelete(e)} style={{fontSize:'16px'}}><i class="fa fa-close"></i> Delete</button>
         </div>
     </div>
     );

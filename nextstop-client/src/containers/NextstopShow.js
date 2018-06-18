33992f741b5291305afd20d6420123e5a5872a13
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-06-18 01:40:29 +0000

    form style

--- nextstop-client/src/containers/NextstopShow.js
+++ nextstop-client/src/containers/NextstopShow.js
@@ -3,6 +3,38 @@ import { connect } from 'react-redux';
 import "./NextstopShow.css";
 
 class NextstopShow extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      comments: [],
+      comment: ''
+    }
+  }
+  handleCommentSubmit = (e) => {
+    e.preventDefault();
+    const addComment=this.state.comment;
+    // const newComment = Object.assign({}, {content: this.state.comment, likes: Math.floor(Math.random() * 100)})
+    // console.log(addComment);
+    this.setState({
+        comments: [...this.state.comments, addComment],
+        comment: ''
+    });
+  }
+  
+  handleOnChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.value)
+    this.setState({
+          comment: e.target.value
+    });
+  }
+
+  renderComments() {
+    return this.state.comments.map((c,index) => 
+        <p key={index}>{c}</p>
+     );
+  }
+  
    render() {
     const { match } = this.props
     // debugger
@@ -12,9 +44,17 @@ class NextstopShow extends Component {
         <h3>{this.props.nextstop.name}</h3>
         <img className="ShowImage" src={this.props.nextstop.img_url} />
         <div className="comments">
-
+           {this.renderComments()}
         </div>
-       
+        <form className="commentForm" onSubmit={this.handleCommentSubmit}>
+          <label>Comment</label>
+          <textarea 
+            onChange={this.handleOnChange}
+            value={this.state.comment}
+            placeholder="Input comment here...">
+          </textarea>
+          <button type="submit">Submit Comment</button>
+        </form>
         
       </div>
     

1967a4dc7991dc6caf9a57af3d04f2d864c2368f
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-06-18 00:06:25 +0000

    add NextstopShow img css

--- nextstop-client/src/containers/NextstopShow.js
+++ nextstop-client/src/containers/NextstopShow.js
@@ -1,33 +1,6 @@
-// import React from 'react';
-// import { connect } from 'react-redux';
- 
-// const NextstopShow = ({ nextstop }) =>
-//   <div>
-//     <h3>Name: {nextstop.name}</h3>
-//   </div>
- 
-// const mapStateToProps = (state, ownProps) => {
-//   const nextstop = state.nextstops.find(nextstop => nextstop.id === ownProps.match.params.nextstopId)
- 
-//   if (nextstop) {
-//     return { nextstop }
-//   } else {
-//     return { nextstop: {} }
-//   }
-// }
- 
-// export default connect(mapStateToProps)(NextstopShow);
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
- 
-// const NextstopShow = props => {
-//   return (
-//     <div>
-//       <h3>Show Component!{this.props}</h3>
-//     </div>
-//   );
-// }
+import "./NextstopShow.css";
 
 class NextstopShow extends Component {
    render() {
@@ -37,7 +10,7 @@ class NextstopShow extends Component {
       
       <div>
         <h3>{this.props.nextstop.name}</h3>
-        <img src={this.props.nextstop.img_url} />
+        <img className="ShowImage" src={this.props.nextstop.img_url} />
         <div className="comments">
 
         </div>
